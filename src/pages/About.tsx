4930ed797fae35f3daea2bc008737d8cc2b38792
Rhenel Jhon Sajol <sajol.rhenel123@gmail.com>
2025-11-26 18:46:05 +0800

    fix

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-50">
+      <p className="text-lg text-slate-600">
+        About content coming soon.
+      </p>
+    </div>
+  );
+}
+
+export default About;
+
