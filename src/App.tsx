81f2dcd7cc1959f8acaa262db13cef76a9c554c7
Rhenel Jhon Sajol <sajol.rhenel123@gmail.com>
2025-11-26 18:35:53 +0800

    rhnl

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import Navbar from './components/common/Navbar';
 import Home from './pages/Home';
 import RecentWorks from './pages/RecentWorks';
@@ -7,24 +7,48 @@ import Magazine from './pages/Magazine';
 import About from './pages/About';
 import FAQ from './pages/FAQ';
 import Capture from './pages/Capture';
+import LoginForAdmin from './Admin/Admin_Login';
+import AdminMain from './Admin/Admin_Main';
+import BookingsAppointments from './components/Admin/Booiking_appointments';
+import ContentManagement from './components/Admin/Content_Management';
+import BlogsStories from './components/Admin/Blog_stories';
+import { Navigate } from 'react-router-dom';
+import RequireAuth from './routes/RequireAuth';
+
+function PublicLayout() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/works" element={<RecentWorks />} />
-            <Route path="/services" element={<Service />} />
-            <Route path="/magazine" element={<Magazine />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/capture" element={<Capture />} />
-          </Routes>
-        </main>
-      </div>
+      <Routes>
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/works" element={<RecentWorks />} />
+          <Route path="/services" element={<Service />} />
+          <Route path="/magazine" element={<Magazine />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/capture" element={<Capture />} />
+        </Route>
+        <Route path="/login" element={<LoginForAdmin />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/admin" element={<AdminMain />}>
+            <Route index element={<Navigate to="bookings" replace />} />
+            <Route path="bookings" element={<BookingsAppointments />} />
+            <Route path="content" element={<ContentManagement />} />
+            <Route path="stories" element={<BlogsStories />} />
+          </Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/components/Admin/Booiking_appointments.tsx
@@ -0,0 +1,7 @@
+export default function Bookings_appointments() {
+    return (
+      <div className="p-4 text-xl font-semibold text0">
+        bookings
+      </div>
+    );
+  }
