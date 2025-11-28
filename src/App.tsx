476e1062cbcb33e6f9a33883ef3ad8108f638c57
Rhenel Jhon Sajol <sajol.rhenel123@gmail.com>
2025-11-28 10:55:16 +0800

    scroll-to-top effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, Outlet, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet, Navigate, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar from './components/common/Navbar';
 import ContactSection from './components/sections/ContactSection';
 import Footer from './components/common/Footer';
@@ -19,6 +20,12 @@ import AddNewProject from './Admin/Admin_components/ContentManagement/WorksColle
 
 // Public layout wrapper (UserLayout)
 function UserLayout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
